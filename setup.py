f3a2073d687ca770ea9e040d190e47778a65a1a4
Santhosh S G <santhosh.sg@promantia.com>
2026-07-31 15:30:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-#from pip.req import parse_requirements
-import re, ast
+import re
+import ast
 
-# get version from __version__ variable in nhance/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+_version_re = re.compile(r"__version__\s*=\s*(.*)")
 
-with open('requirements.txt') as f:
-	install_requires = f.read().strip().split('\n')
+with open("requirements.txt") as f:
+    install_requires = [
+        requirement.strip()
+        for requirement in f.readlines()
+        if requirement.strip()
+        and requirement.strip().lower() != "frappe"
+    ]
 
-with open('nhance/__init__.py', 'rb') as f:
-	version = str(ast.literal_eval(_version_re.search(
-		f.read().decode('utf-8')).group(1)))
-
-
-#requirements = parse_requirements("requirements.txt", session="")
+with open("nhance/__init__.py", "rb") as f:
+    version = str(
+        ast.literal_eval(
+            _version_re.search(
+                f.read().decode("utf-8")
+            ).group(1)
+        )
+    )
 
 setup(
-	name='nhance',
-	version=version,
-	description='Nhance',
-	author='Epoch',
-	author_email='support@epochconsulting.in',
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-	#install_requires=[str(ir.req) for ir in requirements],
-	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
+    name="nhance",
+    version=version,
+    description="Nhance",
+    author="Epoch",
+    author_email="support@epochconsulting.in",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires,
 )
